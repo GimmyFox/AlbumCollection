3f981a2e98423e21fc872b5e01788a119180e8fe
GimmyFox <mguzeev99@gmail.com>
2021-12-30 22:55:48 +0300

    sutup delegate and data source

---
+++ AlbumCollection/Albums/Cells/MyAlbumCells.swift
@@ -0,0 +1,26 @@
+//
+//  MyAlbumCells.swift
+//  AlbumCollection
+//
+//  Created by Maksim Guzeev on 30.12.2021.
+//
+
+
+import UIKit
+
+
+class MyAlbumCells: UICollectionViewCell {
+    
+    static let myAlbumCellId = "MyAlbumCells"
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+    }
+    
+}
